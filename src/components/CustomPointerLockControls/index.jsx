7ac08d3476cdb152e38ddac3e56c58b061b944b5
marcelittle <mcribeiro@inf.ufpel.edu.br>
2022-06-04 03:10:02 -0300

    feat: added audio buffer and personalized camera

--- src/components/CustomPointerLockControls/index.jsx
+++ src/components/CustomPointerLockControls/index.jsx
@@ -1,45 +1,73 @@
-// import { useThree } from "@react-three/fiber";
+import _extends from '@babel/runtime/helpers/esm/extends'
+import { useThree } from '@react-three/fiber'
+import * as React from 'react'
+import { PointerLockControls as PointerLockControls$1 } from './PointerLockControls'
 
-// import { Euler, EventDispatcher, Vector3 } from "three";
+const CustomPointerLockControls = /*#__PURE__*/ React.forwardRef(
+  ({ domElement, selector, onChange, onLock, onUnlock, enabled = true, pointerSpeed = 1, ...props }, ref) => {
+    const { camera, ...rest } = props
+    const get = useThree((state) => state.get)
+    const setEvents = useThree((state) => state.setEvents)
+    const gl = useThree((state) => state.gl)
+    const defaultCamera = useThree((state) => state.camera)
+    const invalidate = useThree((state) => state.invalidate)
+    const events = useThree((state) => state.events)
+    const explCamera = camera || defaultCamera
+    const explDomElement = domElement || events.connected || gl.domElement
+    const [controls] = React.useState(() => new PointerLockControls$1(explCamera, pointerSpeed))
+    React.useEffect(() => {
+      if (enabled) {
+        controls.connect(explDomElement) // Force events to be centered while PLC is active
 
-// function CustomPointerLockControls() {
-//   const camera = useThree((state) => state.camera);
-//   const euler = camera.rotation;
-//   const domElement = document.body;
-//   var isLocked = false;
-//   function onMouseMove(event) {
-//     domElement.requestPointerLock();
+        const oldComputeOffsets = get().events.compute
+        setEvents({
+          compute(event, state) {
+            const offsetX = state.size.width / 2
+            const offsetY = state.size.height / 2
+            state.pointer.set((offsetX / state.size.width) * 2 - 1, -(offsetY / state.size.height) * 2 + 1)
+            state.raycaster.setFromCamera(state.pointer, state.camera)
+          },
+        })
+        return () => {
+          controls.disconnect()
+          setEvents({
+            compute: oldComputeOffsets,
+          })
+        }
+      }
+    }, [enabled, controls])
+    React.useEffect(() => {
+      const callback = (e) => {
+        invalidate()
+        if (onChange) onChange(e)
+      }
 
-//     console.log(event);
-//     // if (scope.isLocked === false) return;
+      controls.addEventListener('change', callback)
+      if (onLock) controls.addEventListener('lock', onLock)
+      if (onUnlock) controls.addEventListener('unlock', onUnlock) // Enforce previous interaction
 
-//     const movementX =
-//       event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-//     const movementY =
-//       event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+      const handler = () => controls.lock()
 
-//     euler.y -= movementX * 0.002 * 0.3;
-//     euler.x -= movementY * 0.002 * 0.3;
+      const elements = selector ? Array.from(document.querySelectorAll(selector)) : [document]
+      elements.forEach((element) => element && element.addEventListener('click', handler))
+      return () => {
+        controls.removeEventListener('change', callback)
+        if (onLock) controls.addEventListener('lock', onLock)
+        if (onUnlock) controls.addEventListener('unlock', onUnlock)
+        elements.forEach((element) => (element ? element.removeEventListener('click', handler) : undefined))
+      }
+    }, [onChange, onLock, onUnlock, selector, controls, invalidate])
+    return /*#__PURE__*/ React.createElement(
+      'primitive',
+      _extends(
+        {
+          ref: ref,
+          object: controls,
+        },
+        rest
+      )
+    )
+  }
+)
 
-//     camera.quaternion.setFromEuler(euler);
-//   }
-//   function onPointerlockChange() {
-//     if (domElement.ownerDocument.pointerLockElement === domElement) {
-//       // scope.dispatchEvent( _lockEvent );
-
-//       isLocked = true;
-//     } else {
-//       // scope.dispatchEvent( _unlockEvent );
-
-//       isLocked = false;
-//     }
-//   }
-
-//   domElement.addEventListener("mousemove", onMouseMove);
-//   domElement.ownerDocument.addEventListener(
-//     "pointerlockchange",
-//     onPointerlockChange
-//   );
-//   return <></>;
-// }
-// export {CustomPointerLockControls};
+export { CustomPointerLockControls }
--- src/components/Plane/index.jsx
+++ src/components/Plane/index.jsx
@@ -1,27 +1,23 @@
-import React from "react";
-import { useTexture } from "@react-three/drei";
-import * as THREE from "three";
+import React from 'react'
+import { useTexture } from '@react-three/drei'
+import * as THREE from 'three'
 
 function Plane() {
   const props = useTexture({
-    map: "textures/Tiles107_1K_Color.jpg",
-    displacementMap: "textures/Tiles107_1K_Displacement.jpg",
-    normalMap: "textures/Tiles107_1K_NormalGL.jpg",
-    roughnessMap: "textures/Tiles107_1K_Roughness.jpg",
-    aoMap: "textures/Tiles107_1K_AmbientOcclusion.jpg",
-  });
+    map: 'textures/Tiles107_1K_Color.jpg',
+    displacementMap: 'textures/Tiles107_1K_Displacement.jpg',
+    normalMap: 'textures/Tiles107_1K_NormalGL.jpg',
+    roughnessMap: 'textures/Tiles107_1K_Roughness.jpg',
+    aoMap: 'textures/Tiles107_1K_AmbientOcclusion.jpg',
+  })
 
-  props.map.wrapS = props.map.wrapT = THREE.RepeatWrapping;
+  props.map.wrapS = props.map.wrapT = THREE.RepeatWrapping
 
   return (
-    <mesh
-      rotation={[-Math.PI / 2, 0, 0]}
-      position={[0, -15, 0]}
-      receiveShadow={true}
-    >
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -15, 0]} receiveShadow={true}>
       <planeBufferGeometry args={[1000, 1000]} />
       <meshPhongMaterial
-        color={"dimgray"}
+        color={'dimgray'}
         receiveShadow
         // {...props}
         map={props.map}
@@ -29,6 +25,6 @@ function Plane() {
         displacementScale={0}
       />
     </mesh>
-  );
+  )
 }
-export { Plane };
+export { Plane }
